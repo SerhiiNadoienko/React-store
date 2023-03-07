56ac96def1a6f550402128041683bc1b7946e36b
Serhii <sergenadoenko@gmail.comm>
2023-03-07 12:04:15 +0100

    second commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import AppRoutes from '../Routes/Routes';
+import Sidebar from '../Sidebar/Sidebar';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="container">
+        <Sidebar />
+        <AppRoutes />
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
